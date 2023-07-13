363cb87e5a5adeaa172d1cbdb8da43ecfa343555
Serhii Leshchenko <sergrut99@gmail.com>
2023-07-13 17:15:14 +0300

    Remove fluent validation library from infrastructure proj

--- src/Randomizer.Infrastructure/DependencyInjection.cs
+++ src/Randomizer.Infrastructure/DependencyInjection.cs
@@ -1,24 +1,16 @@
-﻿using FluentValidation;
-using LynxMarvelTor.BL.Services.FileSystem.Pdf;
+﻿using LynxMarvelTor.BL.Services.FileSystem.Pdf;
 using Microsoft.Extensions.DependencyInjection;
 using PdfSharp.Fonts;
 using Randomizer.Application.Abstractions.Infrastructure;
 using Randomizer.Application.DTOs.FileSystem;
-using Randomizer.Application.Validation;
 using Randomizer.Infrastructure.FileSystem.Pdf;
 using Randomizer.Infrastructure.FileSystem.Pdf.Content;
-using Randomizer.Infrastructure.Validation;
-using System.Reflection;
 
 namespace Randomizer.Infrastructure;
 public static class DependencyInjection
 {
     public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
     {
-        services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly(), ServiceLifetime.Scoped, includeInternalTypes: true);
-        services.AddScoped<IRandomService, RandomService>();
-        services.AddScoped<ICoreValidator, FluentCoreValidator>();
-
         // Pdf services
         GlobalFontSettings.FontResolver = new BasicFontResolver();
         services.AddScoped<BaseContent<GameResultsDocumentDto>, GameResultsContent>();
